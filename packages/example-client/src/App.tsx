35356f7349ac3055302031c6e576eb20ef535698
Filip Pajic <fica.pajic@gmail.com>
2024-05-30 13:48:44 +0200

    chore: package naming update

--- packages/example-client/src/App.tsx
+++ packages/example-client/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Footer} from "footer/src/index.js";
+import {Footer} from "@webcave/example-lib/src";
 
 function App() {
   const [count, setCount] = useState(0)
